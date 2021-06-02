97659402a3329d2455479ebbb89a2697093f1727
Mukhammadamin2002 <abdullaev2002>
2021-06-02 22:58:01 +0500

    Initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<head>
+  <!-- ... --->
+ <meta charset="UTF-8" />
+ <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+ <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body class="bg-gradient-to-r from-gray-100 to gray-200">
+    @yield('content')
+</body>
